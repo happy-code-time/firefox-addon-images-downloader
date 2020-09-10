56b31b062a840ec33e8edf8050bc352e6ef84850
Number9 <david.janitzek@gmail.com>
2020-09-10 20:08:04 +0200

    Added fontawesome localy

--- webpack.dashboard.prod.js
+++ webpack.dashboard.prod.js
@@ -13,7 +13,7 @@ const config = {
     entry: ['babel-polyfill', ENTRY_DASHBOARD.in ],
     output: {
         path: ENTRY_DASHBOARD.out,
-        filename: 'app.bundle.v3.0.0.js',
+        filename: 'app.bundle.v3.0.1.js',
     },
     cache: false,
     devtool: false,
